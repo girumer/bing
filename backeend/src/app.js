176a0fea2e10674bd2bbf69be5790db6379a8d03
girumer <bgirum9@gmail.com>
2025-07-28 02:08:04 +0300

    Initial commit after reinstall

--- backeend/src/app.js
+++ backeend/src/app.js
@@ -38,8 +38,11 @@ const server=http.createServer(app);
 
 
 app.use(bodyParser.json());
+
+
 const allowedOrigins = [
   'http://localhost:3000',
+
   //          // Development URL
   'https://localhost:3000',        // Development URL with HTTPS
   'http://167.235.140.218',        // IP address (if needed)
@@ -47,8 +50,27 @@ const allowedOrigins = [
   'https://adeyebingo.com',        // Production domain with HTTPS
   'http://www.adeyebingo.com',     // www version with HTTP
   'https://www.adeyebingo.com',//      // www version with HTTPS
+
+  'http://167.235.140.218',
+  'http://adeyebingo.com',
+  'https://adeyebingo.com',
+  'http://www.adeyebingo.com',
+  'https://www.adeyebingo.com',
+
   'https://api.adeyebingo.com',
 ];
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+}));
+
+
 console.log("json update");
 
 
