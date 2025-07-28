b6ec38bf6a362747441364a036b0a699c07c5a6f
girumer <bgirum9@gmail.com>
2025-07-28 04:44:53 +0300

    Saving changes before pulling

--- backeend/config/jwtconfig.js
+++ backeend/config/jwtconfig.js
@@ -1,14 +1,10 @@
-const path = require('path');
-const dotenv = require('dotenv');
-
-dotenv.config({ path: path.resolve(__dirname, '/backeend/src/.env') });
+require('dotenv').config();
 const crypto = require('crypto');
 
 // Use the JWT_SECRET from the environment, or fallback to a randomly generated key if not set
 const secretkey = process.env.JWT_SECRET;
 
-const refreshKey=process.env.JWT_PRIVATE;
-console.log('All env vars:', process.env);
+const refreshKey=process.env.JWT_PRIVATE
 console.log('secretkey:', secretkey);  // Should print the value of JWT_SECRET
 console.log('refreshKey:', refreshKey);  // Should print the value of JWT_PRIVATE
 
